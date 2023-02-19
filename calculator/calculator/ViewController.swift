126313807d8263f8d1b79b612f2f7bef205531a1
keremkiyak <110934008+keremkiyak@users.noreply.github.com>
2023-02-19 10:19:54 +0300

    Add files via upload
    i made calculator design

---
+++ calculator/calculator/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by kerem on 19.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstNumberLabel: UILabel!
+    
+    @IBOutlet weak var secondNumberLabel: UILabel!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var firstNumberText: UITextField!
+    
+    @IBOutlet weak var secondNumberText: UITextField!
+    
+    @IBOutlet weak var resultText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func addButton(_ sender: Any) {
+    }
+    
+    @IBAction func minusButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func multiplication(_ sender: Any) {
+    }
+    
+    
+    @IBAction func division(_ sender: Any) {
+    }
+}
+
